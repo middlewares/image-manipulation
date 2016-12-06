8ab4b7ea860f1cc0fb11bd5cc9f023cec2e99d91
oscarotero <oom@oscarotero.com>
2016-12-06 16:24:30 +0100

    updated tests

--- tests/ImageManipulationTest.php
+++ tests/ImageManipulationTest.php
@@ -4,9 +4,7 @@ namespace Middlewares\Tests;
 
 use Middlewares\ImageManipulation;
 use Middlewares\Utils\Dispatcher;
-use Middlewares\Utils\CallableMiddleware;
-use Zend\Diactoros\ServerRequest;
-use Zend\Diactoros\Response;
+use Middlewares\Utils\Factory;
 
 class ImageManipulationTest extends \PHPUnit_Framework_TestCase
 {
@@ -15,21 +13,22 @@ class ImageManipulationTest extends \PHPUnit_Framework_TestCase
         $key = uniqid();
         $path = '/assets/foto.jpg';
         $uri = ImageManipulation::getUri($path, 'resizeCrop,50,50|format,png', $key);
-        $request = (new ServerRequest([], [], '/subdirectory/of/images'.$uri))->withHeader('Accept', 'image/*');
+        $request = Factory::createServerRequest([], 'GET', '/subdirectory/of/images'.$uri)
+            ->withHeader('Accept', 'image/*');
 
         $response = (new Dispatcher([
             new ImageManipulation($key),
-            new CallableMiddleware(function ($request) use ($path) {
+            function ($request) use ($path) {
                 $this->assertEquals('/subdirectory/of/images'.$path, $request->getUri()->getPath());
                 $content = file_get_contents(
                     'https://upload.wikimedia.org/wikipedia/commons/5/58/Vaca_rubia_galega._Oroso_1.jpg'
                 );
 
-                $response = new Response();
+                $response = Factory::createResponse();
                 $response->getBody()->write($content);
 
                 return $response;
-            }),
+            },
         ]))->dispatch($request);
 
         $this->assertEquals('png', pathinfo($uri, PATHINFO_EXTENSION));

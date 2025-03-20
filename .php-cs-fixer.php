18ee829dbfd729ebc2d6fee8acaed180ff420f34
Filis Futsarov <filisfutsarov@gmail.com>
2025-03-20 12:54:12 +0100

    support for PHP 8.4 amongst other improvements

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,6 +3,7 @@
 return My\PhpCsFixerConfig::create()
     ->setFinder(
         PhpCsFixer\Finder::create()
+            // NoUnusedImportsFixer
             ->files()
             ->name('*.php')
             ->in(__DIR__.'/src')
